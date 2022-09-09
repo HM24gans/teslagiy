51b7bca6ab36f0f56c0751c093029a8818544a68
HM24gans <112872541+HM24gans@users.noreply.github.com>
2022-09-09 14:24:10 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,14 +1,9 @@
 let handler = async m => m.reply(`
-◪ 📮 *DONASI DAN SCRIPT BOT*
-│ *Jangan lupa Kasih Donasi Biar Semagat Update*
-│ 「 Donasi • Pulsa/Dana 」
+◪ 📮 *「 Donasi • Pulsa/Dana 」*
 │ • [6285767154127]
-│ *Script BOT :* 
-│ • https://teknosimple.com/qWF6eqsGKZ
-│ • https://teknosimple.com/j4dzEmXzj1
-│ • https://teknosimple.com/qWF6eqsGKZ
-│ *Script Termux Full Ijo Free :* 
-│ • https://teknosimple.com/WxEwnqaa2
+│ • [6285767154137]
+│ *NOTE*
+│ *KLICK DOANG DONASI KAGA*🗿
 ╰──────────═┅═──────────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
