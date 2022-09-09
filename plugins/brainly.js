3df3f811f6d9fdb8a1f5ac659c4e0cc427c131a0
HM24gans <112872541+HM24gans@users.noreply.github.com>
2022-09-09 14:00:47 +0700

    Update brainly.js

--- plugins/brainly.js
+++ plugins/brainly.js
@@ -13,9 +13,9 @@ handler.limit = true
 
 module.exports = handler*/
 
-/*
-const Brainly = require('brainly-scraper-v2')
-const brain = new Brainly("id")
+
+const brainly = require('brainly-scraper-v2')
+const brain = new Brainly("us")
 let handler = async function (m, { text, usedPrefix, command }) {
   if (!text) throw `uhm.. soalnya mana?\n\ncontoh:\n${usedPrefix + command} apa itu javascript?`
   brain.search("id", text).then(async res => {
@@ -23,16 +23,16 @@ let handler = async function (m, { text, usedPrefix, command }) {
     let json = JSON.parse(br)
     let answer = json.map((v, i) => `_*PERTANYAAN KE ${i + 1}*_\n${v.question.content}\n${v.answers.map((v, i) => `*JAWABAN KE ${i + 1}*\n${v.content.replace(/<\/?p>|<\/?strong>|<\/?u>|<\/?h[1-3]>|<\/?span>/g, '').replace(/<br ?(\/|\\)?>/g, '\n')}`).join('\n')}`).join('\n\n•------------•\n\n')
     m.reply(answer)
-  })
-}
+  }
+
 handler.help = ['brainly <soal>']
 handler.tags = ['internet']
 handler.command = /^brainly$/i
 handler.limit = true
 
-module.exports = handler*/
+module.exports = handler
 
-
+/*
 const { MessageType } = require('@adiwajshing/baileys')
 const brainly = require('brainly-scraper')
 let handler = async function (m, { text, usedPrefix, command }) {
@@ -52,7 +52,7 @@ handler.tags = ['internet']
 handler.command = /^brainly$/i
 handler.limit = true
 
-module.exports = handler
+module.exports = handler*/
 
 /*
 const brainly = require('brainly-scraper-v2')
